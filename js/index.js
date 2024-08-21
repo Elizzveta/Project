25c0c4e0772432d710d82093e7ce7282be9efd2b
Elizzveta <elizavetazvanec@mail.ru>
2024-08-21 21:19:24 +0300

    Add css/,js/,img/, index.html, Presentation.pptx

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.slide');
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  const progressBar = document.querySelector('.progress-bar');
+
+  let activeSlide = 0;
+  function nextSlide() {
+    activeSlide = (activeSlide + 1) % slides.length;
+    updateSlider();
+  }
+  function prevSlide() {
+    activeSlide = (activeSlide - 1 + slides.length) % slides.length;
+    updateSlider();
+  }
+  function updateSlider() {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[activeSlide].classList.add('active');
+    progressBar.style.width = `${(activeSlide + 1) / slides.length * 100}%`;
+  }
+
+  prevButton.addEventListener('click', prevSlide);
+  nextButton.addEventListener('click', nextSlide);
+
+  updateSlider();
